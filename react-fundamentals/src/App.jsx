16d83c62283e4e413165d098c2ee4463840e6a58
Alton <smithiialton@gmail.com>
2024-09-29 00:23:02 -0700

    completd problem 2

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import spookyImage from "./photo/spooky.jpg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="header-container">
+        <header>
+          <img src={spookyImage} alt="Jack-O-Lantern" />
+          <p id="header-name">Jack the Lantern</p>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function About() {
+  return (
+    <>
+      <div id="about-container">
+        <h2>About</h2>
+        <p id="about">
+          In the shadowy corners of the haunted forest, a fearsome villain
+          roams: Jack the Lantern, a skeletal figure with a glowing
+          jack-o'-lantern for a head. With hollow eye sockets that flicker like
+          candlelight, he strikes fear into the hearts of those who dare to
+          wander too close. Jack's bony fingers, long and claw-like, clutch a
+          staff entwined with creeping vines, radiating an eerie glow that seems
+          to dance with the spirits of the night. Legends whisper of his cursed
+          existence, bound to protect his domain from intruders, forever seeking
+          vengeance against those who disturb the peace of his eerie realm. As
+          Halloween approaches, the tales of Jack the Lantern spread like
+          wildfire, warning all to stay away from the flickering lights that
+          lure the unsuspecting into his grasp.
+        </p>
+      </div>
+    </>
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About /> 
+    </>
+  );
+}
+
+
+export default App;
+
